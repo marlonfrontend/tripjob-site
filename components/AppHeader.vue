5b6a0789d4ec3f51d13aa9c660ef20044f6385c6
Johnnatan <contatojohnnatan@gmail.com>
2020-07-21 21:55:46 -0300

    Modify menu and banner

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,31 +1,50 @@
 <template>
-  <header class="bg-primary py-4">
+  <div class="allcontain">
+  <header class="bg-primary py-6">
     <div class="container mx-auto">
       <div class="flex justify-between items-center">
         <div class="logo text-white">
-          test
+          <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clip-path="url(#clip0)">
+          <path d="M36 0L0 20.25L11.5049 24.5111L29.2501 7.87502L15.7525 26.0844L15.7635 26.0885L15.75 26.0844V36.0001L22.2019 28.4729L30.3751 31.5001L36 0Z" fill="white"/>
+          </g>
+          <defs>
+          <clipPath id="clip0">
+          <rect width="36" height="36" fill="white"/>
+          </clipPath>
+          </defs>
+          </svg>
         </div>
         <div class="flex items-center">
-          <nav class="text-white">
+          <nav class="text-white mr-3">
             <ul class="flex">
-              <li class="mx-2">
-                <nuxt-link to="/">home</nuxt-link>
+              <li class="mx-4">
+                <nuxt-link to="/" class="font-sans hover:text-secundary">Home</nuxt-link>
               </li>
-              <li class="mx-2">
-                <nuxt-link to="/preco">planos</nuxt-link>
+              <li class="mx-4">
+                <nuxt-link to="/produto" class="font-sans hover:text-secundary">Produto</nuxt-link>
+              </li>
+              <li class="mx-4">
+                <nuxt-link to="/solucoes" class="font-sans hover:text-secundary">Soluções</nuxt-link>
+              </li>
+              <li class="mx-4">
+                <nuxt-link to="/precos" class="font-sans hover:text-secundary">Preços</nuxt-link>
               </li>
             </ul>
           </nav>
-          <button class="hover:bg-white hover:text-secundary text-white border-solid border border-white rounded-md py-5 px-12">
+          <div class="mr-1">
+          <button class="font-sans text-white hover:bg-white hover:text-secundary border-solid border border-white rounded-md py-5 px-12 ml-3 mr-3">
+            Entrar
+          </button>
+          <button class="font-sans text-white bg-secundary hover:bg-white hover:text-primary rounded-md py-5 px-10">
             Cadastrar
           </button>
-          <button class="text-white bg-secundary rounded-md py-5 px-12 ml-5">
-            Cadastrar
-          </button>
+          </div>
         </div>
       </div>
     </div>
   </header>
+  </div>
 </template>
 
 <script>
@@ -37,10 +56,7 @@ export default {
   .nuxt-link-exact-active {
     @apply text-secundary;
   }
-  .navbar.vue-fixed-header--isFixed {
-    position: fixed;
-    left: 0;
-    top: 0;
-    width: 100vw;
-  }
+
+
+
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,24 +4,8 @@
       <div class="container mx-auto">
         <div class="flex items-center flex-wrap">
           <div class="md:w-1/2 w-full">
-            <h1 class="text-5xl"></h1>
           </div>
         </div>
-      </div>
-    </div>
-
-    <button class="bg-secundary" @click="fn">
-      teste
-    </button>
-
-    <div class="produtos" v-if="value">
-      <div class="container mx-auto">
-        <ul class="flex flex-wrap">
-          <li v-for="(item, index) in 8" :key="index">
-            <h2>teste</h2>
-            <img src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQNDheRTiCdtRy0WiLOVW_5JzvP1Hy0N8V5mLnmyrkMOoJFGG9XVzW-DTY_cUAk1UVEUjQJ2wYbs4YmZ2nnl0gkf0uSvg_FLyQtE5TOWXoN&usqp=CAE" alt="">
-          </li>
-        </ul>
       </div>
     </div>
   </main>
@@ -29,17 +13,16 @@
 
 <script>
 export default {
-  data: () => ({
-    value: true
-  }),
-  methods: {
-    fn () {
-      this.value = !this.value;
-    }
-  }
+
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.allcontain {
+  background-image: url(~assets/img/banner.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  opacity: 25%;
+  height: 75vh;
+}
 </style>
