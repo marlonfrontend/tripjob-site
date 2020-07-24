7aaad3b1d6515056d42e3ac532ba1d6ecfb2ef36
Johnnatan <contatojohnnatan@gmail.com>
2020-07-24 16:44:01 -0300

    Modified main page banner

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,21 +1,11 @@
 <template>
-    <div class="allcontain">
         <header class="py-6" :class="{ 'fixed': fixed }">
             <div class="container mx-auto">
                 <div class="flex justify-between items-center">
                     <div class="logo text-white">
-                        <svg
-                            width="36"
-                            height="36"
-                            viewBox="0 0 36 36"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
+                        <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <g clip-path="url(#clip0)">
-                                <path
-                                    d="M36 0L0 20.25L11.5049 24.5111L29.2501 7.87502L15.7525 26.0844L15.7635 26.0885L15.75 26.0844V36.0001L22.2019 28.4729L30.3751 31.5001L36 0Z"
-                                    fill="white"
-                                />
+                                <path d="M36 0L0 20.25L11.5049 24.5111L29.2501 7.87502L15.7525 26.0844L15.7635 26.0885L15.75 26.0844V36.0001L22.2019 28.4729L30.3751 31.5001L36 0Z" fill="white"/>
                             </g>
                             <defs>
                                 <clipPath id="clip0">
@@ -31,38 +21,24 @@
                                     <nuxt-link to="/" class="font-sans hover:text-secundary">Home</nuxt-link>
                                 </li>
                                 <li class="mx-4">
-                                    <nuxt-link
-                                        to="/produto"
-                                        class="font-sans hover:text-secundary"
-                                    >Produto</nuxt-link>
+                                    <nuxt-link to="/produto" class="font-sans hover:text-secundary">Produto</nuxt-link>
                                 </li>
                                 <li class="mx-4">
-                                    <nuxt-link
-                                        to="/solucoes"
-                                        class="font-sans hover:text-secundary"
-                                    >Soluções</nuxt-link>
+                                    <nuxt-link to="/solucoes" class="font-sans hover:text-secundary">Soluções</nuxt-link>
                                 </li>
                                 <li class="mx-4">
-                                    <nuxt-link
-                                        to="/precos"
-                                        class="font-sans hover:text-secundary"
-                                    >Preços</nuxt-link>
+                                    <nuxt-link to="/precos" class="font-sans hover:text-secundary">Preços</nuxt-link>
                                 </li>
                             </ul>
                         </nav>
                         <div class="mr-1">
-                            <button
-                                class="font-sans text-white hover:bg-white hover:text-secundary border-solid border border-white rounded-md py-5 px-12 ml-3 mr-3"
-                            >Entrar</button>
-                            <button
-                                class="font-sans text-white bg-secundary hover:bg-white hover:text-primary rounded-md py-5 px-10"
-                            >Cadastrar</button>
+                            <button class="font-sans text-white hover:bg-white hover:text-secundary border-solid border border-white rounded-md py-4 px-12 ml-3 mr-3">Entrar</button>
+                            <button class="font-sans text-white bg-secundary hover:bg-white hover:text-primary rounded-md py-4 px-10">Cadastrar</button>
                         </div>
                     </div>
                 </div>
             </div>
         </header>
-    </div>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,9 @@
       <div class="container mx-auto">
         <div class="flex items-center flex-wrap">
           <div class="md:w-1/2 w-full">
+            <h1 class="relative pt-48 text-white text-5xl font-sans font-hairline">Soluções de viagens<br/>para empresas</h1>
+            <h3 class="relative pt-5 text-white text-base font-sans font-hairline">Plataforma "ponta a ponta" com gestão de viagens e despesas em uma única solução. Economize tempo e dinheiro com hotéis, passagens aéreas e aluguéis de carro.</h3>
+            <button class="relative font-sans text-white bg-secundary hover:bg-white hover:text-primary rounded-md py-4 px-10 mt-5">Cadastrar</button>
           </div>
         </div>
       </div>
@@ -33,4 +36,5 @@ export default {
     right: 0;
   }
 }
+
 </style>
